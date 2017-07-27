5c4eebf13a08ba2fcbeddeebdce223fbb27e96c6
Shota Matsuda <shota@takram.com>
2017-07-28 06:15:21 +0900

    enabled extend option in rollup

--- dist/planck-worker.js
+++ dist/planck-worker.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 	typeof define === 'function' && define.amd ? define(['exports'], factory) :
-	(factory((global.Planck = {})));
+	(factory((global.Planck = global.Planck || {})));
 }(this, (function (exports) { 'use strict';
 
 //
